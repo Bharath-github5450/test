dfd72ac525c4d383315b649b73a26dab276f79ac
owner <owner@192.168.0.40>
2020-05-24 20:04:59 -0400

    test checkin

---
+++ demo/src/main/java/com/example/demo/Input.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+
+
+public class Input {
+	
+	
+	private Integer fv;
+	public Integer getFv() {
+		return fv;
+	}
+	public void setFv(Integer fv) {
+		this.fv = fv;
+	}
+	public Integer getR() {
+		return r;
+	}
+	public void setR(Integer r) {
+		this.r = r;
+	}
+	public Integer getN() {
+		return n;
+	}
+	public void setN(Integer n) {
+		this.n = n;
+	}
+	private Integer r;
+	private Integer n;
+
+private Integer pv;
+public Integer getPv() {
+	return pv;
+}
+public void setPv(Integer pv) {
+	this.pv = pv;
+}
+
+}
